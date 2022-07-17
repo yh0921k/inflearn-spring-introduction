8e2af530664f9b76163b3eab5aa065646934c06c
Yonghwi Kim <yh0921k@gmail.com>
2022-07-18 00:02:26 +0900

    add: Section 07. 스프링 DB 접근 기술
    - 스프링 통합 테스트

---
+++ src/test/java/study/spring_introduction/service/MemberServiceIntegrationTest.java
@@ -0,0 +1,52 @@
+package study.spring_introduction.service;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import study.spring_introduction.domain.Member;
+import study.spring_introduction.repository.JdbcMemberRepository;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@SpringBootTest
+@Transactional
+class MemberServiceIntegrationTest {
+
+  @Autowired MemberService memberService;
+  @Autowired JdbcMemberRepository memberRepository;
+
+  @Test
+  void 회원가입() {
+    // given
+    Member member = new Member();
+    member.setName("Spring");
+
+    // when
+    Long savedId = memberService.join(member);
+
+    // then
+    Member foundMember = memberService.findOne(savedId).get();
+    assertThat(member.getName()).isEqualTo(foundMember.getName());
+  }
+
+  @Test
+  public void 중복_회원_예외() {
+    // given
+    Member member1 = new Member();
+    member1.setName("Spring");
+
+    Member member2 = new Member();
+    member2.setName("Spring");
+
+    // when
+    memberService.join(member1);
+
+    // then
+    IllegalArgumentException exception =
+        assertThrows(IllegalArgumentException.class, () -> memberService.join(member2));
+    assertThat(exception.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
+  }
+}
